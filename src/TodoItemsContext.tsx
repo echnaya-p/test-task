e1fb460e2c517b753366ec29233b53ce73148d15
Polina <p.echnaya@gmail.com>
2021-11-19 11:23:35 +0300

    added husky, prettier, lint-staged

--- src/TodoItemsContext.tsx
+++ src/TodoItemsContext.tsx
@@ -1,142 +1,115 @@
-import {
-    createContext,
-    ReactNode,
-    useContext,
-    useEffect,
-    useReducer,
-} from 'react';
+import { createContext, ReactNode, useContext, useEffect, useReducer } from 'react';
 
 export interface TodoItem {
-    id: string;
-    title: string;
-    details?: string;
-    done: boolean;
+	id: string;
+	title: string;
+	details?: string;
+	done: boolean;
 }
 
 interface TodoItemsState {
-    todoItems: TodoItem[];
+	todoItems: TodoItem[];
 }
 
 interface TodoItemsAction {
-    type: 'loadState' | 'add' | 'delete' | 'toggleDone' | 'reorder' | 'edit';
-    data: any;
+	type: 'loadState' | 'add' | 'delete' | 'toggleDone' | 'reorder' | 'edit';
+	data: any;
 }
 
-const TodoItemsContext = createContext<
-    (TodoItemsState & { dispatch: (action: TodoItemsAction) => void }) | null
->(null);
+const TodoItemsContext = createContext<(TodoItemsState & { dispatch: (action: TodoItemsAction) => void }) | null>(null);
 
 const defaultState = { todoItems: [] };
 const localStorageKey = 'todoListState';
 
-export const TodoItemsContextProvider = ({
-    children,
-}: {
-    children?: ReactNode;
-}) => {
-    const [state, dispatch] = useReducer(todoItemsReducer, defaultState);
+export const TodoItemsContextProvider = ({ children }: { children?: ReactNode }) => {
+	const [state, dispatch] = useReducer(todoItemsReducer, defaultState);
 
-    useEffect(() => {
-        const savedState = localStorage.getItem(localStorageKey);
+	useEffect(() => {
+		const savedState = localStorage.getItem(localStorageKey);
 
-        if (savedState) {
-            try {
-                dispatch({ type: 'loadState', data: JSON.parse(savedState) });
-            } catch {}
-        }
-    }, []);
+		if (savedState) {
+			try {
+				dispatch({ type: 'loadState', data: JSON.parse(savedState) });
+			} catch {}
+		}
+	}, []);
 
-    useEffect(() => {
-        localStorage.setItem(localStorageKey, JSON.stringify(state));
-    }, [state]);
+	useEffect(() => {
+		localStorage.setItem(localStorageKey, JSON.stringify(state));
+	}, [state]);
 
-    return (
-        <TodoItemsContext.Provider value={{ ...state, dispatch }}>
-            {children}
-        </TodoItemsContext.Provider>
-    );
+	return <TodoItemsContext.Provider value={{ ...state, dispatch }}>{children}</TodoItemsContext.Provider>;
 };
 
 export const useTodoItems = () => {
-    const todoItemsContext = useContext(TodoItemsContext);
+	const todoItemsContext = useContext(TodoItemsContext);
 
-    if (!todoItemsContext) {
-        throw new Error(
-            'useTodoItems hook should only be used inside TodoItemsContextProvider',
-        );
-    }
+	if (!todoItemsContext) {
+		throw new Error('useTodoItems hook should only be used inside TodoItemsContextProvider');
+	}
 
-    return todoItemsContext;
+	return todoItemsContext;
 };
 
 function todoItemsReducer(state: TodoItemsState, action: TodoItemsAction) {
-    switch (action.type) {
-        case 'loadState': {
-            return action.data;
-        }
-        case 'add':
-            return {
-                ...state,
-                todoItems: [
-                    { id: generateId(), done: false, ...action.data.todoItem },
-                    ...state.todoItems,
-                ],
-            };
-        case 'delete':
-            return {
-                ...state,
-                todoItems: state.todoItems.filter(
-                    ({ id }) => id !== action.data.id,
-                ),
-            };
-        case 'toggleDone':
-            const itemIndex = state.todoItems.findIndex(
-                ({ id }) => id === action.data.id,
-            );
-            const item = state.todoItems[itemIndex];
+	switch (action.type) {
+		case 'loadState': {
+			return action.data;
+		}
+		case 'add':
+			return {
+				...state,
+				todoItems: [{ id: generateId(), done: false, ...action.data.todoItem }, ...state.todoItems],
+			};
+		case 'delete':
+			return {
+				...state,
+				todoItems: state.todoItems.filter(({ id }) => id !== action.data.id),
+			};
+		case 'toggleDone':
+			const itemIndex = state.todoItems.findIndex(({ id }) => id === action.data.id);
+			const item = state.todoItems[itemIndex];
 
-            return {
-                ...state,
-                todoItems: [
-                    ...state.todoItems.slice(0, itemIndex),
-                    { ...item, done: !item.done },
-                    ...state.todoItems.slice(itemIndex + 1),
-                ],
-            };
-        case 'reorder': {
-            return {
-                ...state,
-                todoItems: action.data,
-            };
-        }
+			return {
+				...state,
+				todoItems: [
+					...state.todoItems.slice(0, itemIndex),
+					{ ...item, done: !item.done },
+					...state.todoItems.slice(itemIndex + 1),
+				],
+			};
+		case 'reorder': {
+			return {
+				...state,
+				todoItems: action.data,
+			};
+		}
 
-        case 'edit': {
-            const updatedTodoItems = state.todoItems.map((item) => {
-                if (item.id === action.data.id) {
-                    return {
-                        id: action.data.id,
-                        title: action.data.title,
-                        details: action.data.details,
-                        done: action.data.done,
-                    }
-                }
+		case 'edit': {
+			const updatedTodoItems = state.todoItems.map(item => {
+				if (item.id === action.data.id) {
+					return {
+						id: action.data.id,
+						title: action.data.title,
+						details: action.data.details,
+						done: action.data.done,
+					};
+				}
 
-                return item
-            });
+				return item;
+			});
 
-            return {
-                ...state,
-                todoItems: updatedTodoItems,
-            };
-        }
+			return {
+				...state,
+				todoItems: updatedTodoItems,
+			};
+		}
 
-        default:
-            throw new Error();
-    }
+		default:
+			throw new Error();
+	}
 }
 
 function generateId() {
-    return `${Date.now().toString(36)}-${Math.floor(
-        Math.random() * 1e16,
-    ).toString(36)}`;
+	return `${Date.now().toString(36)}-${Math.floor(Math.random() * 1e16).toString(36)}`;
 }
